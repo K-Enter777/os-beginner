e493e39f6f262ff2d19feccdd74437000f95ebd9
K-Enter <58861743+K-Enter777@users.noreply.github.com>
2023-11-28 20:33:23 +0900

    Day1 modified (#5)
    * uefi-rs を使うように変更
    
    * 日記: 1日目に追記

--- 01/rust/src/main.rs
+++ 01/rust/src/main.rs
@@ -1,27 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-use r_efi::efi;
-use utf16_literal::utf16;
+use uefi::prelude::*;
 
-#[export_name = "efi_main"]
-pub extern "C" fn efi_main(
-    _image_handle: efi::Handle,
-    system_table: *mut efi::SystemTable,
-) -> efi::Status {
-    let buf = utf16!("Hello, world!\n\0");
+#[entry]
+fn efi_main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
 
-    unsafe {
-        ((*(*system_table).con_out).output_string)(
-            (*system_table).con_out,
-            buf.as_ptr() as *mut efi::Char16,
-        )
-    };
-    loop {}
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+    system_table
+        .stdout()
+        .output_string(cstr16!("Hello, World!\r\n"))
+        .unwrap();
     loop {}
 }
